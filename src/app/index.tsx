685fe26ae1cbb0f640101b8be2585fd9c74366aa
Flávio <f.augustosdn@gmail.com>
2024-02-14 13:27:22 -0300

    feat(layout): agora ao selecionar uma das categorias, ela está ficando com a borda indicando que está selecionada

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,15 @@ import { View, FlatList } from "react-native";
 import { CATEGORIES } from "@/src/utils/data/products";
 import { Header } from "../components/header";
 import { CategoryButton } from "../components/category-button";
+import { useState } from "react";
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelected(categorySelected: string) {
+    setCategory(categorySelected);
+  }
+
   return (
     <View className="pt-8">
       <Header title="Faça o seu pedido" cartQuantityItems={2} />
@@ -11,7 +18,13 @@ export default function Home() {
       <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <CategoryButton title={item} />}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelected(item)}
+          />
+        )}
         horizontal
         className="max-h-10 mt-5"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
