e830c8a9073231596712ecfb7a8d51142a30dacb
Flávio <f.augustosdn@gmail.com>
2024-02-14 15:49:31 -0300

    feat(layout): agora está sendo listados os produtos na sua determinada categoria

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,9 @@
-import { View, FlatList } from "react-native";
-import { CATEGORIES } from "@/src/utils/data/products";
+import { View, FlatList, SectionList, Text } from "react-native";
+import { CATEGORIES, MENU } from "@/src/utils/data/products";
 import { Header } from "../components/header";
 import { CategoryButton } from "../components/category-button";
 import { useState } from "react";
+import { Product } from "../components/product";
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0]);
@@ -12,7 +13,7 @@ export default function Home() {
   }
 
   return (
-    <View className="pt-8">
+    <View className="flex-1 pt-8">
       <Header title="Faça o seu pedido" cartQuantityItems={2} />
 
       <FlatList
@@ -29,6 +30,21 @@ export default function Home() {
         className="max-h-10 mt-5"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
         showsHorizontalScrollIndicator={false}
+      />
+
+      <SectionList
+        className="flex-1 p-5"
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => <Product data={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   );
